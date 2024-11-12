97b21bf40c56c8cb9021e65b5343ce2f6d979a9d
michaeldymock25 <michael.dymock1@hotmail.com>
2024-11-12 14:35:32 +0800

    Update to case study using ENBS functions.

--- Examples/RSV/figures.R
+++ Examples/RSV/figures.R
@@ -1,9 +1,41 @@
 
 ## this script produces the figures for the RSV case study in the manuscript
-## the code in RSV.Rmd must be run first
+## the functions from the ValueAdapt package must be loaded the code in RSV.Rmd must be run first
 
-library(dplyr)
+library(ValueAdapt)
 library(ggplot2)
+library(dplyr)
+
+set.seed(12856421)
+
+n <- seq(50, 10000, by = 50)
+out_perfect <- vector(length = length(n))
+out_sample <- vector(length = length(n))
+for(i in 1:length(n)){
+  print(n[i])
+  out_perfect[i] <- enb_perfect(D = D, U = U, Theta = prior, t = t_0, prop = prop, cost = delta + gamma*n[i])
+  samp_fun <- function(Theta)
+    matrix(c(rbinom(nrow(Theta), size = n[i], prob = Theta[,"II"]),
+             rbinom(nrow(Theta), size = n[i], prob = Theta[,"MV"])),
+           nrow = nrow(Theta), ncol = 2, dimnames = list(NULL, c("II", "MV")))
+  out_sample[i] <- enb_sample(D = D, U = U, Theta = prior, t = t_0, prop = prop, cost = delta + gamma*n[i],
+                              samp_fun = samp_fun, stat_fun = stat_fun, model = model)
+}
+dat_vis <- data.frame(n = rep(n, 2),
+                      value = c(out_sample, out_perfect),
+                      grp = rep(c("Sample information", "Perfect information"), each = length(n)))
+
+jpeg("Examples/RSV/Figure 1.jpg", width = 6, height = 7, units = "in", res = 1000)
+
+ggplot(dat_vis, aes(x = n, y = value, linetype = grp)) +
+  geom_line() +
+  scale_x_continuous("Proposed trial sample size per arm", n.breaks = 5) +
+  scale_y_continuous("ENBS", n.breaks = 5) +
+  scale_linetype_manual("", values = c("dashed", "solid")) +
+  theme_bw() +
+  theme(legend.position = "bottom")
+
+dev.off()
 
 post_vis <- data.frame(Analysis = 0,
                        value = prior[,"II"] - prior[,"MV"])
@@ -22,7 +54,7 @@ post_vis <- bind_rows(post_vis |> mutate(Scenario = "Scenario 1"),
 post_vis$Analysis <- factor(post_vis$Analysis, labels = c("Prior", "Analysis 1", "Analysis 2", "Analysis 3", "Analysis 4"))
 post_vis$int <- ifelse(post_vis$Scenario == "Scenario 1", 0.08, 0)
 
-jpeg("Examples/RSV/Figure 1.jpg", width = 6, height = 7, units = "in", res = 1000)
+jpeg("Examples/RSV/Figure 2.jpg", width = 6, height = 7, units = "in", res = 1000)
 
 ggplot(post_vis, aes(x = value)) +
   facet_grid(Analysis ~ Scenario) +
@@ -41,7 +73,7 @@ net_benefit_vis <- bind_rows(net_benefit_vis |> mutate(Scenario = "Scenario 1"),
                              INMB_2 |> mutate(Scenario = "Scenario 2") |> rename(INMB = x))
 net_benefit_vis$Analysis <- factor(net_benefit_vis$Analysis, levels = c("Prior", "Analysis 1", "Analysis 2", "Analysis 3", "Analysis 4"))
 
-jpeg("Examples/RSV/Figure 2.jpg", width = 6, height = 7, units = "in", res = 1000)
+jpeg("Examples/RSV/Figure 3.jpg", width = 6, height = 7, units = "in", res = 1000)
 
 ggplot(net_benefit_vis, aes(x = INMB)) +
   facet_grid(Analysis ~ Scenario) +
@@ -54,7 +86,7 @@ ggplot(net_benefit_vis, aes(x = INMB)) +
 
 dev.off()
 
-jpeg("Examples/RSV/Figure 3.jpg", width = 6, height = 7, units = "in", res = 1000)
+jpeg("Examples/RSV/Figure 4.jpg", width = 6, height = 7, units = "in", res = 1000)
 
 ggplot(sims_summ, aes(x = Analysis, y = Prop, group = Scenario, colour = Scenario)) +
   geom_line() +
--- Examples/RSV/simulations.R
+++ Examples/RSV/simulations.R
@@ -1,31 +1,41 @@
 
 ## this script contains the simulations required to run the RSV case study (RSV.Rmd)
+## the functions from the ValueAdapt package must be loaded
 
 library(ValueAdapt)
 library(here)
 
-set.seed(356723556)
+set.seed(12856421)
 
 D <- 2
-U <- function(d, Theta, lambda = 5200){
-  cost <- ifelse(d == 1, 590, 330)
-  1/1000000*sum(1.05^(1-(1:15)))*300000*(-lambda*Theta[,d] - cost)
+U <- function(d, Theta, t_1, t_2, lambda = 5200){
+  cost <- c(590, 330)
+  constant <- 1/1000000*sum(1.05^(1-(t_1:t_2)))*300000
+  constant*(lambda*(1 - Theta[,d]) - cost[d])
 }
-prior <- matrix(rep(c(4, 10), D), nrow = D, byrow = TRUE)
-n_analyses <- 4
+t <- matrix(c(0, 2, 15,
+              2, 3, 15,
+              3, 4, 15),
+            nrow = 3, ncol = 3, byrow = TRUE, dimnames = list(NULL, c("C", "A", "H")))
+prop <- c(1,0)
+cost <- c(1 + 0.002*1000, 0.002*500, 0.002*500)
+prior_par <- matrix(rep(c(4, 10), D), nrow = D, byrow = TRUE)
+n_analyses <- 2
 n_samp <- 250
 n_sims <- 10000
 n_cores <- 10
 
-sims <- list(`Scenario 1` = sim_betabinomial_trial(D = D, U = U, Theta = c(0.10, 0.18), prior = prior, n_analyses = n_analyses,
-                                                   n_samp = n_samp, n_sims = n_sims, n_cores = n_cores),
-             `Scenario 2` = sim_betabinomial_trial(D = D, U = U, Theta = c(0.10, 0.10), prior = prior, n_analyses = n_analyses,
-                                                   n_samp = n_samp, n_sims = n_sims, n_cores = n_cores))
+sims <- list(`Scenario 1` = sim_betabinomial_trial(D = D, U = U, Theta = c(0.10, 0.18), t = t, prop = prop, cost = cost,
+                                                   prior_par = prior_par, n_analyses = n_analyses, n_samp = n_samp,
+                                                   n_sims = n_sims, n_cores = n_cores),
+             `Scenario 2` = sim_betabinomial_trial(D = D, U = U, Theta = c(0.10, 0.12), t = t, prop = prop, cost = cost,
+                                                   prior_par = prior_par, n_analyses = n_analyses, n_samp = n_samp,
+                                                   n_sims = n_sims, n_cores = n_cores))
 
-sims <- data.frame(EVSI = unlist(sims),
-                   Scenario = rep(paste("Scenario", 1:2), each = n_analyses*n_sims),
-                   Analysis = rep(paste("Analysis", 1:n_analyses), 2*n_sims),
-                   Simulation = rep(rep(paste("Simulation", 1:n_sims), each = n_analyses), 2))
+sims <- data.frame(ENBS = unlist(sims),
+                   Scenario = rep(paste("Scenario", 1:2), each = (n_analyses + 1)*n_sims),
+                   Analysis = rep(paste("Analysis", 0:n_analyses), 2*n_sims),
+                   Simulation = rep(rep(paste("Simulation", 1:n_sims), each = n_analyses + 1), 2))
 rownames(sims) <- NULL
 
 saveRDS(sims, here("Examples/RSV/simulations.rds"))
